2bc861720527fc974ed02fc68c8fb94607f72f02
Romain Meniane <romain.meniane@grenoble-inp.org>
2026-03-27 00:14:17 +0100

    q7) tp3, univers qui contient (2^5)^3 particules

--- test/tp3.cpp
+++ test/tp3.cpp
@@ -1,6 +1,8 @@
 #include "Univers.hpp"
 #include <cmath>
 #include <random>
+#include <chrono>
+#include <iostream>
 
 // q6) tp3, univers qui contient (2^5)^3 particules
 // uniformément distribuées sur le cube [0, 1]^3
@@ -22,4 +24,22 @@ int main(){
         Particule p = Particule(i, "particule", 1, pos, Vecteur(0.0, 0.0, 0.0));
         particules.push_back(p);
     }
+
+
+    /// question 6) et 7) de test de l'univers
+
+    const int N_side = 32; // (2^5)
+    const int total_particles = N_side * N_side * N_side; // 32 768
+
+    auto start = std::chrono::steady_clock::now();
+
+    std::deque<Particule> test_container;
+    for (int i = 0; i < total_particles; ++i) {
+        // Insertion d'une particule type
+        test_container.push_back(Particule(i, "Test", 1.0, {0,0,0}, {0,0,0}));
+    }
+
+    auto end = std::chrono::steady_clock::now();
+    std::chrono::duration<double> elapsed = end - start;
+    std::cout << "Temps pour insérer " << total_particles << " particules dans deque: " << elapsed.count() << "s" << std::endl;
 }
