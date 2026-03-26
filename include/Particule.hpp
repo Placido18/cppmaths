9e2ca3fef577c0d9d67f45b7597c5ee260d693ab
Placide Neuilly <Placide.Neuilly@grenoble-inp.org>
2026-03-26 19:39:04 +0100

    particule refait

--- include/Particule.hpp
+++ include/Particule.hpp
@@ -2,19 +2,37 @@
 #define PARTICULE_HPP
 
 #include <string>
+#include <array>
+
+using Vec2 = std::array<double, 2>;
 
 class Particule{
     private:
-        double x, y;
-        double vitesse;
+        int id;
+        std::string type;
         double masse;
-        std::string id;
-        std::string categorie;
-        std::string force;
+        Vec2 position;
+        Vec2 vitesse;
+        Vec2 force;
 
     public:
         void afficher() const;
-        Particule(std::string id, double vitesse, double x, double y, double m);
+        Particule(int id, std::string type, double masse, Vec2 pos, Vec2 vitesse);
+
+        // Getters
+        int getId() const;
+        std::string getType() const;
+        double getMasse() const;
+        Vec2 getPosition() const;
+        Vec2 getVitesse() const;
+        Vec2 getForce() const; 
+        
+        // Setters
+        void setPosition(const Vec2& pos);
+        void setVitesse(const Vec2& vitesse);
+        void setForce(const Vec2& f);
+
+        void ajouterForce(Vec2& f);
 };
 
 #endif
--- src/Particule.cpp
+++ src/Particule.cpp
@@ -1,16 +1,33 @@
 #include "Particule.hpp"
-#include <iostream>
 
-Particule::Particule(std::string id, double vitesse, double x, double y, double m) {
-    this->x = x;
-    this->y = y;
+Particule::Particule(int id, std::string type, double masse, Vec2 pos, Vec2 vitesse) {
     this->id = id;
+    this->type = type;
+    this->masse = masse;
+    this->position = pos;
     this->vitesse = vitesse;
-    this->masse = m;
-    this->categorie = "simple";
-    this->force = "gravitationnel";
+    this->force = {0.0, 0.0};
 }
 
-void Particule::afficher() const {
-    std::cout << "Particule : " << id << ", Masse : " << masse << ", pos : (" << x << ", " << y << ")" << std::endl;
+void Particule::afficher() const{
+
+}
+
+// Getters
+int Particule::getId() const { return id; }
+std::string Particule::getType() const { return type; }
+double Particule::getMasse() const { return masse; }
+Vec2 Particule::getPosition() const { return position; }
+Vec2 Particule::getVitesse() const { return vitesse; }
+Vec2 Particule::getForce() const { return force; }
+
+// Setters
+void Particule::setPosition(const Vec2& pos) { position = pos; }
+void Particule::setVitesse(const Vec2& vit) { vitesse = vit; }
+void Particule::setForce(const Vec2& f) { force = f; }
+
+void Particule::ajouterForce(Vec2& f) {
+    force[0] += f[0];
+    force[1] += f[1];
+    force[2] += f[2];
 }
