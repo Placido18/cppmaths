73d21a77d980ceca405017bc713c4e94cc53f657
Romain Meniane <romain.meniane@grenoble-inp.org>
2026-04-26 17:37:57 +0200

    Q1_TP4_lennard_jones_potential

---
+++ src/lennardJonesPotential.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cmath>
+#include <fstream>
+
+double calculLennardJones(double r, double epsilon, double sigma) {
+    //on prefere r*r pour les puissances moins couteux que la fonction pow
+    if (r==0) {
+        return 0;
+    }
+    double r2 = r * r;
+    double sigma2 = sigma * sigma;
+    double sigma6 = sigma2 * sigma2 * sigma2; // sigma^6
+    double sigma12 = sigma6 * sigma6; // sigma^12
+    double r6 = r2 * r2 * r2; 
+    double r12 = r6 * r6; 
+    return 4 * epsilon * (sigma12 / r12 - sigma6 / r6);
+}
+
+int main() {
+    double epsilon = 1;
+    double sigma = 1;
+
+    double r_min = 0;
+    double r_max = 5;
+
+    double pas_r = 0.1;
+
+    std::ofstream fichier("lennard_jones_potential.txt");
+    if (!fichier.is_open()) {
+        std::cerr << "Erreur : Impossible de créer le fichier !" << std::endl;
+        return 1;
+    }
+
+    for (double r = r_min; r <= r_max; r += pas_r) {
+        double potential = calculLennardJones(r, epsilon, sigma);
+        fichier << r << " " << potential << "\n";
+    }
+
+    fichier.close();
+
+    return 0;
+}
